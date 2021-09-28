60dc6faea58a547300cd0a7515c92d678bc148bc
Jordan Hart <p22jhart@fairfieldprep.org>
2021-09-28 07:04:17 -0700

    lab

---
+++ lab_2-1.py
@@ -0,0 +1,6 @@
+#_author_= JH  (AMDG) 9/28/21
+
+x = input("input an integer: ")
+
+if int(x) % 2 == 0: 
+    print (x + "is an even number")
---
+++ lab_2-2.py
@@ -0,0 +1,9 @@
+#_author_= JH  (AMDG) 9/28/21
+
+x = input("input an integer: ")
+
+if int(x) % 2 == 0: 
+    print (x + "is an even number")
+
+else:
+    print (x + "is an odd number")
