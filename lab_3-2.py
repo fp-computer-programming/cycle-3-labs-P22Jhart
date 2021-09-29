5ca5736592397e3cc9c0787c0c974c58bdbeefe0
Jordan Hart <p22jhart@fairfieldprep.org>
2021-09-29 06:52:21 -0700

    lab

---
+++ lab_3-2.py
@@ -0,0 +1,14 @@
+if  x >= 15:
+    print("Won Gold ")
+    else: 
+        if x >= 12: 
+        print("silver medal")
+    else: 
+        if x < 9:
+        print("No medal!")
+        else:
+            print("they won bronze medal")
+
+            x = int(input("how many points did the team score?"))
+
+     
